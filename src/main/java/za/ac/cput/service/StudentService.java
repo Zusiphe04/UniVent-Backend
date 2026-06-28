d8f730effa68f65091ba2f88e5906976abb73771
samthepessimist <231116144@mycput.ac.za>
2026-06-28 20:46:47 +0200

    added constructor dependency injection

--- src/main/java/za/ac/cput/service/StudentService.java
+++ src/main/java/za/ac/cput/service/StudentService.java
@@ -2,8 +2,16 @@ package za.ac.cput.service;
 
 import org.springframework.stereotype.Service;
 import za.ac.cput.domain.Student;
+import za.ac.cput.repository.StudentRepository;
+
 @Service
 public class StudentService implements IStudentService{
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     @Override
     public Student findByStudentNumber(String studentNumber) {
         return null;
