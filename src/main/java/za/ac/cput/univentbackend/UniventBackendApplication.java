05375b29b98d86ca67c27263c396375f665cc840
samthepessimist <231116144@mycput.ac.za>
2026-06-28 19:41:06 +0200

    initial commit

---
+++ src/main/java/za/ac/cput/univentbackend/UniventBackendApplication.java
@@ -0,0 +1,13 @@
+package za.ac.cput.univentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniventBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniventBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/za/ac/cput/univentbackend/UniventBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package za.ac.cput.univentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniventBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
